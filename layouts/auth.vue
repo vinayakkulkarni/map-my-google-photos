deae3c9e841cc8db871d92a1e80ca6ea8656df00
Vinayak Kulkarni <inbox.vinayak@gmail.com>
2020-11-09 03:02:19 +0530

    fix: add Auth layout

---
+++ layouts/auth.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="w-screen h-screen">
+    <nuxt />
+  </div>
+</template>
+
+<script lang="ts">
+  import { defineComponent } from '@vue/composition-api';
+
+  export default defineComponent({
+    name: 'AuthLayout',
+    auth: false,
+  });
+</script>
